830e7fe55f10f208ddfdeaab27a7060954114477
amit ghosh <amitbd1508@gmail.com>
2017-02-21 02:05:06 +0600

    Initial commit

---
+++ src/homeasisstance/ComPortHandler.java
@@ -0,0 +1,59 @@
+package homeasisstance;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+/**
+ * Created by BlackFlag on 2/21/2017.
+ */
+public class ComPortHandler {
+
+    public static String[] getComPortsList() {
+        SerialPort[] serialPorts = SerialPort.getCommPorts();
+        int len = serialPorts.length;
+        String[] ports = new String[len];
+        for (int i = 0; i < len; ++i) {
+            ports[i] = serialPorts[i].getSystemPortName();
+        }
+        return ports;
+    }
+    public static SerialPort getSerialPort(String deviceName) {
+        return SerialPort.getCommPort(deviceName);
+    }
+
+    public static void readPort(final String comPort) {
+        Thread t = new Thread(){
+
+            @Override
+            public void run() {
+                SerialPort serialPort = SerialPort.getCommPort(comPort);
+                if (serialPort == null) {
+                    return;
+                }
+                serialPort.setBaudRate(115200);
+                serialPort.setComPortTimeouts(0, 0, 0);
+                if (serialPort.openPort()) {
+                    try {
+                        do {
+                            byte[] bytes = new byte[serialPort.bytesAvailable()];
+                            serialPort.readBytes(bytes, bytes.length);
+                            String portData = new String(bytes);
+                            if (portData.trim().isEmpty()) continue;
+                            System.out.println(portData);
+                        } while (true);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    ComPortHandler.sendToConsole("Port Closed");
+                }
+                serialPort.closePort();
+            }
+        };
+        t.start();
+    }
+    public static void sendToConsole(String msg) {
+        System.out.println(msg);
+    }
+
+}
---
+++ src/homeasisstance/Main.java
@@ -0,0 +1,10 @@
+package homeasisstance;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
